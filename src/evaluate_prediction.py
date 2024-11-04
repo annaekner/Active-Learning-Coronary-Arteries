bffab3631a025e16d369d5ffc71113c4b1d1aebd
annaekner <s193396@student.dtu.dk>
2024-11-04 16:23:21 +0100

    retraining in batches

--- src/evaluate_prediction.py
+++ src/evaluate_prediction.py
@@ -2,7 +2,7 @@ import hydra
 import logging
 
 from load_save_utilities import load_sample, load_prediction, save_prediction_centerline_to_vtk
-from tools import compute_centerline_from_prediction, compute_evaluation_metrics_wrtGTsegmentation, compute_evaluation_metrics_wrtGTcenterline, select_sample_for_retraining
+from tools import compute_centerline_from_prediction, compute_evaluation_metrics_wrtGTsegmentation, compute_evaluation_metrics_wrtGTcenterline, select_samples_for_retraining
 
 # Set up logging
 log = logging.getLogger(__name__)
@@ -11,9 +11,9 @@ log = logging.getLogger(__name__)
 def main(config):
     
     # TODO: These variable need to be set!
-    index = 4           # Set the index (in file_list_test.txt) of the sample to load
+    index = 5           # Set the index (in file_list_test.txt) of the sample to load
     all_indices = True  # Set to True to evaluate all predictions, or False to evaluate a single prediction
-    selection_method = 'best'  # Method for selecting sample for retraining ('worst', 'best', 'random')
+    selection_method = 'worst'  # Method for selecting sample for retraining ('worst', 'best', 'random')
 
     # --------------------- Evaluate a single prediction --------------------- #
     if not all_indices:
@@ -30,7 +30,7 @@ def main(config):
         assert ground_truth_segmentation.shape == prediction_segmentation.shape, 'Ground truth and prediction must have the same shape'
 
         # 3. Compute and save centerline from the predicted LAD segmentation
-        prediction_centerline_indices = compute_centerline_from_prediction(prediction_segmentation, prediction_segmentation_nii, img_index, config)
+        prediction_centerline_indices = compute_centerline_from_prediction(prediction_segmentation, config)
         save_prediction_centerline_to_vtk(prediction_centerline_indices, prediction_segmentation_nii, img_index, config)
 
         # 4. Evaluate the prediction (w.r.t the ground truth LAD segmentation, when available)
@@ -69,7 +69,7 @@ def main(config):
             prediction_segmentation, prediction_segmentation_nii = load_prediction(img_index, config)
 
             # 3. Compute centerline from the predicted LAD segmentation
-            prediction_centerline_indices = compute_centerline_from_prediction(prediction_segmentation, prediction_segmentation_nii, img_index, config)
+            prediction_centerline_indices = compute_centerline_from_prediction(prediction_segmentation, config)
 
             # 4. Evaluate the prediction (w.r.t. the ground truth LAD centerline, which is always available)
             evaluation_metrics_centerline = compute_evaluation_metrics_wrtGTcenterline(ground_truth_centerline_indices, prediction_centerline_indices, prediction_segmentation, img_index, log, config)
@@ -77,21 +77,11 @@ def main(config):
             # 5. Store evaluation metrics in nested dictionary
             evaluation_metrics_all[img_index] = evaluation_metrics_centerline
 
-        # Select sample for retraining
-        retraining = select_sample_for_retraining(evaluation_metrics_all, selection_method, log, config)
-
         # Set logging level back to INFO
         log.setLevel(logging.INFO)
 
-        log.info(f'-------------------------------- Re-training -------------------------------')
-        log.info(f'Number of samples evaluated: {num_samples}')
-        log.info(f'Image index of sample selected for re-training: {retraining["sample_for_retraining"]}')
-        log.info(f'Selection method: {retraining["selection_method"]}')
-        log.info(f'Number of connected components: {retraining["num_connected_components"]}')
-        log.info(f'Predicted centerline coverage of the ground truth centerline: {retraining["predicted_centerline_coverage_of_ground_truth_centerline"]:.4f}')
-        log.info(f'Ground truth centerline coverage of the predicted centerline: {retraining["ground_truth_centerline_coverage_of_predicted_centerline"]:.4f}')
-        log.info(f'Combined centerline "DICE" score: {retraining["combined_centerline_dice_score"]:.4f}')
-        log.info(f'----------------------------------------------------------------------------\n')
+        # Select sample for retraining
+        retraining = select_samples_for_retraining(evaluation_metrics_all, selection_method, log, config)
 
 if __name__ == "__main__":
     main()
--- src/tools.py
+++ src/tools.py
@@ -63,7 +63,7 @@ def compute_centerline_distance_map(sample):
 
     return distance_map
 
-def compute_connected_components(segmentation):
+def compute_connected_components(segmentation, config):
     """
     Compute the connected components of given segmentation (e.g. prediction).
 
@@ -73,16 +73,34 @@ def compute_connected_components(segmentation):
         labeled_array: Array of same shape as the segmentation, where each voxel has a 
                        value corresponding to which connected component it belongs to,
                        either 0 = background, 1 = first connected component, 2 = second 
-                       connected component, and so on. I.e. if there is only one connected 
+                       connected component, and so on. E.g. if there is only one connected 
                        component, background voxels have value 0 and connected component voxels have value 1.
 
         largest_cc_array: Array of same shape as the segmentation, where each voxel has a 
                           value of 1 if it belongs to the largest connected component, and 0 otherwise.
     """
-    labeled_array, num_connected_components = scipy.ndimage.label(segmentation)
-    largest_cc_array = (labeled_array == 1).astype(int)
 
-    # TODO: Only consider connected components over a certain size, see Rasmus' code
+    # Configuration settings
+    min_size = config.connected_components.min_size
+
+    # Compute connected components
+    labeled_array, num_connected_components = scipy.ndimage.label(segmentation)
+    components_sizes = np.unique(labeled_array, return_counts=True)[1] # Voxels per connected component
+
+    # Find largest connected component
+    largest_cc_index = np.argmax(components_sizes)
+    largest_cc_array = (labeled_array == largest_cc_index).astype(int)
+
+    # Remove small connected components
+    if len(components_sizes) > 2:
+
+        # Loop over connected components (excluding background, which has label 0)
+        for i in range(1, num_connected_components + 1):
+
+            # Remove connected components smaller than min_size
+            if components_sizes[i] < min_size:
+                labeled_array[labeled_array == i] = 0 
+                num_connected_components -= 1
 
     return num_connected_components, labeled_array, largest_cc_array
 
@@ -98,7 +116,7 @@ def compute_tp_fp_fn_tn(ground_truth, prediction):
 
     return tp, fp, fn, tn
 
-def compute_centerline_from_prediction(prediction, prediction_nii, img_index, config):
+def compute_centerline_from_prediction(prediction, config):
     """
     Compute the centerline from the predicted LAD segmentation using skeletonization.
     """
@@ -199,10 +217,11 @@ def compute_evaluation_metrics_wrtGTcenterline(ground_truth_centerline_indices, 
     """
 
     # Connected components of prediction
-    num_connected_components, labeled_array, largest_cc_array = compute_connected_components(prediction_segmentation)
+    num_connected_components, labeled_array, largest_cc_array = compute_connected_components(prediction_segmentation, config)
 
-    # Get tolerance from config
+    # Configuration settings
     tolerance = config.centerline_predictions.tolerance
+    min_size = config.connected_components.min_size
 
     # Get unique indices of the ground truth centerline
     ground_truth_centerline_indices = np.unique(ground_truth_centerline_indices, axis = 0)
@@ -217,8 +236,9 @@ def compute_evaluation_metrics_wrtGTcenterline(ground_truth_centerline_indices, 
     # Log evaluation metrics
     log.info(f'--------------- Evaluation metrics (w.r.t GT LAD centerline) ---------------')
     log.info(f'Image index: {img_index}')
-    log.warning(f'Number of connected components: {num_connected_components} (image {img_index}') if num_connected_components > 1 \
+    log.warning(f'Number of connected components: {num_connected_components} (image {img_index})') if num_connected_components > 1 \
         else log.info(f'Number of connected components: {num_connected_components}')
+    log.info(f'Minimum size for connected components: {min_size} voxels')
     log.info(f'Number of points in predicted centerline: {len(prediction_centerline_indices)}')
     log.info(f'Number of points in ground truth centerline: {len(ground_truth_centerline_indices)}')
     log.info(f'Tolerance for centerline coverage: {tolerance} voxels')
@@ -236,48 +256,108 @@ def compute_evaluation_metrics_wrtGTcenterline(ground_truth_centerline_indices, 
     
     return evaluation_metrics
 
-def select_sample_for_retraining(evaluation_metrics_all, selection_method, log, config):
+def select_samples_for_retraining(evaluation_metrics_all, selection_method, log, config):
     """
-    Select sample for retraining based on the evaluation metrics of all predictions.
+    Select n samples for retraining based on the evaluation metrics of all predictions.
 
     Args
         evaluation_metrics_all: Nested dictionary of evaluation metrics for all predictions.
         selection_method: Method for selecting the sample for retraining ('worst', 'best', 'random').
     """
-    # List of samples for retraining
-    samples_considered_for_retraining = []
-    
-    # Find all predictions with more than one connected component (if any)
-    for img_index in evaluation_metrics_all:
-        if evaluation_metrics_all[img_index]['num_connected_components'] > 1:
-            samples_considered_for_retraining.append(img_index)
-    
-    # If no predictions had > 1 connected component, all predictions are considered for retraining
-    if not samples_considered_for_retraining:
-        samples_considered_for_retraining = list(evaluation_metrics_all.keys())
 
-    # print(f'Samples considered for retraining: {samples_considered_for_retraining}')
+    # Configuration settings
+    num_samples_per_retraining = config.retraining.num_samples_per_retraining
+
+    # List of samples considered for retraining
+    samples_considered_for_retraining = []
+
+    # All samples sorted by number of connected components (in ascending and descending order)
+    num_samples = len(evaluation_metrics_all)
+    all_samples = list(evaluation_metrics_all.keys())
+    all_samples_DICE_ascending = sorted(all_samples, key=lambda x: evaluation_metrics_all[x]['combined_centerline_dice_score'], reverse=False)
+    all_samples_DICE_descending = sorted(all_samples, key=lambda x: evaluation_metrics_all[x]['combined_centerline_dice_score'], reverse=True)
 
     if selection_method == 'worst':
-        # Select sample with the lowest combined centerline "DICE" score
-        sample_for_retraining = min(samples_considered_for_retraining, key=lambda x: evaluation_metrics_all[x]['combined_centerline_dice_score'])
+        # Find all predictions with > 1 connected component (if any)
+        for img_index in evaluation_metrics_all:
+            if evaluation_metrics_all[img_index]['num_connected_components'] > 1:
+                samples_considered_for_retraining.append(img_index)
+
+        # If not enough samples have > 1 connected components, add more
+        if len(samples_considered_for_retraining) < num_samples_per_retraining:
+            extra_samples = num_samples_per_retraining - len(samples_considered_for_retraining)
+            
+            for img_index in all_samples_DICE_ascending:
+
+                # Add sample to list of samples considered for retraining if it has not already been added
+                if img_index not in samples_considered_for_retraining:
+                    samples_considered_for_retraining.append(img_index)
+                    extra_samples -= 1
+
+                if extra_samples == 0:
+                    break
+
+        if len(samples_considered_for_retraining) == num_samples_per_retraining:
+            samples_for_retraining = samples_considered_for_retraining
+        
+        else:
+            # Select sample with the lowest combined centerline "DICE" score
+            samples_for_retraining = sorted(samples_considered_for_retraining, 
+                                        key=lambda x: evaluation_metrics_all[x]['combined_centerline_dice_score'],
+                                        reverse=False)[:num_samples_per_retraining]
 
     elif selection_method == 'best':
-        # Select sample with the highest combined centerline "DICE" score
-        sample_for_retraining = max(samples_considered_for_retraining, key=lambda x: evaluation_metrics_all[x]['combined_centerline_dice_score'])
+        # Find all predictions with exactly 1 connected component (if any)
+        for img_index in evaluation_metrics_all:
+            if evaluation_metrics_all[img_index]['num_connected_components'] == 1:
+                samples_considered_for_retraining.append(img_index)
+
+        # If not enough samples have exactly 1 connected components, add more
+        if len(samples_considered_for_retraining) < num_samples_per_retraining:
+            extra_samples = num_samples_per_retraining - len(samples_considered_for_retraining)
+
+            for img_index in all_samples_DICE_descending:
+
+                # Add sample to list of samples considered for retraining if it has not already been added
+                if img_index not in samples_considered_for_retraining:
+                    samples_considered_for_retraining.append(img_index)
+                    extra_samples -= 1
+
+                if extra_samples == 0:
+                    break
+
+        if len(samples_considered_for_retraining) == num_samples_per_retraining:
+            samples_for_retraining = samples_considered_for_retraining
+
+        else:
+            # Select sample with the highest combined centerline "DICE" score
+            samples_for_retraining = sorted(samples_considered_for_retraining, 
+                                        key=lambda x: evaluation_metrics_all[x]['combined_centerline_dice_score'], 
+                                        reverse=True)[:num_samples_per_retraining]
 
     elif selection_method == 'random':
         # Select random sample
-        sample_for_retraining = np.random.default_rng(seed = 0).choice(samples_considered_for_retraining)
+        samples_for_retraining = np.random.default_rng(seed = 0).choice(all_samples, num_samples_per_retraining, replace=False)
 
     retraining = {
-                  'sample_for_retraining': sample_for_retraining,
+                  'samples_for_retraining': samples_for_retraining,
                   'selection_method': selection_method,
-                  'num_connected_components': evaluation_metrics_all[sample_for_retraining]["num_connected_components"],
-                  'predicted_centerline_coverage_of_ground_truth_centerline': evaluation_metrics_all[sample_for_retraining]["predicted_centerline_coverage_of_ground_truth_centerline"],
-                  'ground_truth_centerline_coverage_of_predicted_centerline': evaluation_metrics_all[sample_for_retraining]["ground_truth_centerline_coverage_of_predicted_centerline"],
-                  'combined_centerline_dice_score': evaluation_metrics_all[sample_for_retraining]["combined_centerline_dice_score"],
+                  'num_connected_components': [evaluation_metrics_all[sample]["num_connected_components"] for sample in samples_for_retraining],
+                  'predicted_centerline_coverage_of_ground_truth_centerline': [round(evaluation_metrics_all[sample]["predicted_centerline_coverage_of_ground_truth_centerline"], 4) for sample in samples_for_retraining],
+                  'ground_truth_centerline_coverage_of_predicted_centerline': [round(evaluation_metrics_all[sample]["ground_truth_centerline_coverage_of_predicted_centerline"], 4) for sample in samples_for_retraining],
+                  'combined_centerline_dice_score': [round(evaluation_metrics_all[sample]["combined_centerline_dice_score"], 4) for sample in samples_for_retraining],
                  }
+
+    log.info(f'-------------------------------- Re-training -------------------------------')
+    log.info(f'Number of samples evaluated: {num_samples}')
+    log.info(f'Number of samples selected for re-training: {num_samples_per_iteration}')
+    log.info(f'Selection method: {retraining["selection_method"]}')
+    log.info(f'Image indices of samples selected for re-training: {retraining["samples_for_retraining"]}')
+    log.info(f'Number of connected components: {retraining["num_connected_components"]}')
+    log.info(f'Predicted centerline coverage of the ground truth centerline: {retraining["predicted_centerline_coverage_of_ground_truth_centerline"]}')
+    log.info(f'Ground truth centerline coverage of the predicted centerline: {retraining["ground_truth_centerline_coverage_of_predicted_centerline"]}')
+    log.info(f'Combined centerline "DICE" score: {retraining["combined_centerline_dice_score"]}')
+    log.info(f'----------------------------------------------------------------------------\n')
 
     return retraining
 
